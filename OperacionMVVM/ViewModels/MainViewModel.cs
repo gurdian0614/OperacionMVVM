25127acc7e5643f237fb0f306c5e757a4bc4b5ad
gurdian0614 <ivan.gurdian@outlook.com>
2023-12-09 00:46:12 -0600

    Add project files.

---
+++ OperacionMVVM/ViewModels/MainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
+namespace OperacionMVVM.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private double valor1;
+        
+        [ObservableProperty] 
+        private double valor2;
+
+        [ObservableProperty]
+        private double resultado;
+
+        [RelayCommand]
+        private void Division()
+        {
+            try
+            {
+                if (Valor2 != 0)
+                {
+                    Resultado = Valor1 / Valor2;
+                } else
+                {
+                    MainThread.BeginInvokeOnMainThread(async () => await App.Current!.MainPage!.DisplayAlert("ERROR", "No se puede dividir entre cero", "Aceptar"));
+                }
+            } catch (Exception ex)
+            {
+                MainThread.BeginInvokeOnMainThread(async () => await App.Current!.MainPage!.DisplayAlert("ERROR", ex.Message, "Aceptar"));
+            }
+        }
+
+        [RelayCommand]
+        private void Limpiar()
+        {
+            Valor1 = Valor2 = Resultado = 0;
+        }    }
+}
